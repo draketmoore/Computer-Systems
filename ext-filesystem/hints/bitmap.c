371ae33acd8254a47fd5000f26c81c499a13e506
Drake Moore <drakem@gmail.com>
2021-03-04 11:57:34 -0800

    Add files via upload

---
+++ ext-filesystem/hints/bitmap.c
@@ -0,0 +1,19 @@
+// based on cs3650 starter code
+
+#include "bitmap.h"
+
+int
+bitmap_get(void* bm, int ii)
+{
+	return 0;
+}
+
+void
+bitmap_put(void* bm, int ii, int vv)
+{
+}
+
+void
+bitmap_print(void* bm, int size)
+{
+}
